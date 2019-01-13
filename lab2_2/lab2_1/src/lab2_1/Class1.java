671ab6f984c023fade39880d1e20601cf47a193f
philipMartini <45294280+philipMartini@users.noreply.github.com>
2019-01-13 13:01:27 +0100

    Add files via upload

---
+++ lab2_2/lab2_1/src/lab2_1/Class1.java
@@ -0,0 +1,21 @@
+package lab2_1;
+
+public class Class1 {
+	
+	public static void main(String[] args){
+		//Comment
+		//Added Another Comment
+		System.out.println("Hello Wolrld");
+	}
+	
+	public void firstMethod(){
+		
+	}
+	
+	public void secondMethod(){}
+	
+	
+	
+}
+	
+
